cd84fac5632aae36fd1e8c63797e8f9a6d6e1fa8
jstakun <jstakun@redhat.com>
2013-10-02 21:28:01 +0200

    Minor changes

--- GMSLib/src/com/jstakun/gms/android/landmarks/GooglePlacesReader.java
+++ GMSLib/src/com/jstakun/gms/android/landmarks/GooglePlacesReader.java
@@ -5,7 +5,6 @@
 
 package com.jstakun.gms.android.landmarks;
 
-import com.jstakun.gms.android.config.Commons;
 import com.jstakun.gms.android.config.ConfigurationManager;
 import com.jstakun.gms.android.utils.GMSAsyncTask;
 import java.util.List;
@@ -15,14 +14,23 @@ import java.util.Locale;
  *
  * @author jstakun
  */
-public class GooglePlacesReader extends AbstractJsonReader {
+public class GooglePlacesReader extends AbstractSerialReader {
 
-    @Override
+    /*@Override
     public String readRemoteLayer(List<ExtendedLandmark> landmarks, double latitude, double longitude, int zoom, int width, int height, String layer, GMSAsyncTask<?, ? ,?> task) {
         init(latitude, longitude, zoom, width, height);
         String lang = Locale.getDefault().getLanguage();
         String url = ConfigurationManager.getInstance().getServicesUrl() + "googlePlacesProvider?latitude=" + coords[0] + 
                 "&longitude=" + coords[1] + "&radius=" + (radius*1000) + "&language=" + lang + "&limit=" + limit + "&display=" + display + "&version=2";
         return parser.parse(url, landmarks, Commons.GOOGLE_PLACES_LAYER, null, -1, -1, task, true, limit);
-    }
+    }*/
+
+	@Override
+	protected String readLayer(List<ExtendedLandmark> landmarks,
+			double latitude, double longitude, int zoom, int width, int height, String layer, GMSAsyncTask<?, ?, ?> task) {
+		String lang = Locale.getDefault().getLanguage();
+		String url = ConfigurationManager.getInstance().getServicesUrl() + "googlePlacesProvider?latitude=" + coords[0] + 
+                "&longitude=" + coords[1] + "&radius=" + (radius*1000) + "&language=" + lang + "&limit=" + limit + "&display=" + display + "&version=" + SERIAL_VERSION + "&format=bin";
+		return parser.parse(url, landmarks, task, true, null);
+	}
 }
--- GMSLib/src/com/jstakun/gms/android/landmarks/TwitterReader.java
+++ GMSLib/src/com/jstakun/gms/android/landmarks/TwitterReader.java
@@ -4,7 +4,6 @@
  */
 package com.jstakun.gms.android.landmarks;
 
-import com.jstakun.gms.android.config.Commons;
 import com.jstakun.gms.android.config.ConfigurationManager;
 import com.jstakun.gms.android.utils.GMSAsyncTask;
 import java.util.List;
@@ -13,14 +12,23 @@ import java.util.List;
  *
  * @author jstakun
  */
-public class TwitterReader extends AbstractJsonReader {
+public class TwitterReader extends AbstractSerialReader {
 
-    @Override
+    /*@Override
     public String readRemoteLayer(List<ExtendedLandmark> landmarks, double latitude, double longitude, int zoom, int width, int height, String layer, GMSAsyncTask<?, ? ,?> task) {
         init(latitude, longitude, zoom, width, height);
         String url = ConfigurationManager.getInstance().getServicesUrl() + "twitterProvider?lat=" + coords[0] + 
                 "&lng=" + coords[1] + "&radius=" + radius + "&limit=" + limit + "&display=" + display;
         return parser.parse(url, landmarks, Commons.TWITTER_LAYER, null, -1, -1, task, true, limit);
-    }
+    }*/
+
+	@Override
+	protected String readLayer(List<ExtendedLandmark> landmarks,
+			double latitude, double longitude, int zoom, int width, int height,
+			String layer, GMSAsyncTask<?, ?, ?> task) {
+		String url = ConfigurationManager.getInstance().getServicesUrl() + "twitterProvider?lat=" + coords[0] + 
+                "&lng=" + coords[1] + "&radius=" + radius + "&limit=" + limit + "&display=" + display + "&version=" + SERIAL_VERSION + "&format=bin";
+		return parser.parse(url, landmarks, task, true, null);
+	}
     
 }
