26e36e2165f67fcf6f0118ef78575f7dc7de790e
jstakun <jstakun@redhat.com>
2013-12-12 22:38:07 +0100

    Frebasee layer added

--- GMSLib/src/com/jstakun/gms/android/config/ConfigurationManager.java
+++ GMSLib/src/com/jstakun/gms/android/config/ConfigurationManager.java
@@ -326,17 +326,17 @@ public final class ConfigurationManager {
 
     public String getServicesUrl() {
         if (getUserManager().isUserLoggedIn()) {
-            return SERVER_SERVICES_URL; //"http://10.0.2.2:8888/services/";
+            return SERVER_SERVICES_URL; //"http://10.0.2.2:8080/services/"; //
         } else {
-            return SERVER_URL; //"http://10.0.2.2:8888/";
+            return SERVER_URL; //"http://10.0.2.2:8080/"; //
         }
     }
 
     public String getSecuredServicesUrl() {
         if (getUserManager().isUserLoggedIn()) {
-            return SSL_SERVER_SERVICES_URL; //"http://10.0.2.2:8888/services/"; //
+            return SSL_SERVER_SERVICES_URL; //"http://10.0.2.2:8080/services/"; //
         } else {
-            return SSL_SERVER_URL; //"http://10.0.2.2:8888/"; //
+            return SSL_SERVER_URL; //"http://10.0.2.2:8080/"; // 
         }
     }
 
