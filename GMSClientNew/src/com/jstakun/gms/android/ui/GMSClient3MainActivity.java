a040bc13fdf9ba6e4e6faf51ace0eef763ab5458
jstakun <jstakun@gms-world.net>
2016-08-03 18:30:31 +0200

    initial commit of new version based on google maps api v2

---
+++ GMSClientNew/src/com/jstakun/gms/android/ui/GMSClient3MainActivity.java
@@ -0,0 +1,236 @@
+package com.jstakun.gms.android.ui;
+
+import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.jstakun.gms.android.config.Commons;
+import com.jstakun.gms.android.landmarks.ExtendedLandmark;
+import com.jstakun.gms.android.landmarks.LandmarkFactory;
+import com.openlapi.QualifiedCoordinates;
+
+public class GMSClient3MainActivity extends ActionBarActivity implements NavigationDrawerFragment.NavigationDrawerCallbacks, OnMapReadyCallback {
+
+	private static final int SHOW_MAP_VIEW = 0;
+	
+	private Handler loadingHandler;
+	private GoogleMarkerClusterOverlay markerCluster;
+    
+	private NavigationDrawerFragment mNavigationDrawerFragment;
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gmsclient3_main);
+
+        mNavigationDrawerFragment = (NavigationDrawerFragment) getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
+    
+        // Set up the drawer.
+        mNavigationDrawerFragment.setUp(R.id.navigation_drawer, (DrawerLayout) findViewById(R.id.drawer_layout));
+        
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this); 
+        
+        loadingHandler = new LoadingHandler(this);
+        
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+        	actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
+            actionBar.setDisplayShowTitleEnabled(true);
+            actionBar.setTitle(getTitle());
+            actionBar.hide();
+        }
+        
+        //loadingHandler.sendEmptyMessageDelayed(SHOW_MAP_VIEW, 5000);
+    }
+
+    @Override
+    public void onNavigationDrawerItemSelected(int position) {
+        // update the main content by replacing fragments
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.mapContainer, PlaceholderFragment.newInstance(position + 1))
+                .commit();
+    }
+
+    /*public void onSectionAttached(int number) {
+        switch (number) {
+            case 1:
+                mTitle = getString(R.string.title_section1);
+                break;
+            case 2:
+                mTitle = getString(R.string.title_section2);
+                break;
+            case 3:
+                mTitle = getString(R.string.title_section3);
+                break;
+        }
+    }*/
+
+    public void restoreActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
+        actionBar.setDisplayShowTitleEnabled(true);
+        actionBar.setTitle(getTitle());
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        if (!mNavigationDrawerFragment.isDrawerOpen()) {
+            // Only show items in the action bar relevant to this screen
+            // if the drawer is not showing. Otherwise, let the drawer
+            // decide what to show in the action bar.
+            getMenuInflater().inflate(R.menu.main_menu_2, menu);
+            //restoreActionBar();
+            return true;
+        }
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        //if (id == R.id.action_settings) {
+        //    return true;
+        //}
+        return super.onOptionsItemSelected(item);
+    }
+
+    //
+    // A placeholder fragment containing a simple view.
+    //
+    public static class PlaceholderFragment extends Fragment {
+        //
+        // The fragment argument representing the section number for this
+        // fragment.
+        //
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        //
+        // Returns a new instance of this fragment for the given section
+        // number.
+        //
+        public static PlaceholderFragment newInstance(int sectionNumber) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.gmsclient3_fragment_main, container, false);
+            return rootView;
+        }
+
+        @Override
+        public void onAttach(Activity activity) {
+            super.onAttach(activity);
+            //((GMSClient3MainActivity) activity).onSectionAttached(
+            //        getArguments().getInt(ARG_SECTION_NUMBER));
+        }
+    }
+
+	@Override
+	public void onMapReady(GoogleMap map) {
+		this.mMap = map;
+		LatLng latLng = new LatLng(52.25, 20.95);
+	    CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 15);
+	    mMap.moveCamera(cameraUpdate);
+	    loadingHandler.sendEmptyMessage(SHOW_MAP_VIEW);
+	    markerCluster = new GoogleMarkerClusterOverlay(this, mMap);
+	    
+	    //TODO load overlays here
+	    List<ExtendedLandmark> default_locations = new ArrayList<ExtendedLandmark>();
+	    long installed = System.currentTimeMillis();
+	    default_locations.add(LandmarkFactory.getLandmark("United States, Los Angeles", "", new QualifiedCoordinates(34.052234, -118.243685, 0f, Float.NaN, Float.NaN), Commons.LOCAL_LAYER, installed)); //United States Los Angeles 34.052234,-118.243685
+		default_locations.add(LandmarkFactory.getLandmark("United States, New York", "", new QualifiedCoordinates(40.71427, -74.00597, 0f, Float.NaN, Float.NaN), Commons.LOCAL_LAYER, installed)); //United States Los Angeles 34.052234,-118.243685
+		default_locations.add(LandmarkFactory.getLandmark("United States, San Francisco", "", new QualifiedCoordinates(37.77493, -122.41942, 0f, Float.NaN, Float.NaN), Commons.LOCAL_LAYER, installed)); //United States Los Angeles 34.052234,-118.243685
+		default_locations.add(LandmarkFactory.getLandmark("France, Paris", "", new QualifiedCoordinates(48.856918, 2.34121, 0f, Float.NaN, Float.NaN), Commons.LOCAL_LAYER, installed)); //France, Paris 48.856918, 2.34121 
+		default_locations.add(LandmarkFactory.getLandmark("Germany, Berlin", "", new QualifiedCoordinates(52.516071, 13.37698, 0f, Float.NaN, Float.NaN), Commons.LOCAL_LAYER, installed)); //Germany, Berlin 52.516071, 13.37698 
+		default_locations.add(LandmarkFactory.getLandmark("Italy, Rome", "", new QualifiedCoordinates(41.901514, 12.460774, 0f, Float.NaN, Float.NaN), Commons.LOCAL_LAYER, installed)); //Italy, Rome 41.901514, 12.460774
+		default_locations.add(LandmarkFactory.getLandmark("Spain, Madrid", "", new QualifiedCoordinates(40.4203, -3.70577, 0f, Float.NaN, Float.NaN), Commons.LOCAL_LAYER, installed)); //Spain, Madrid 40.4203,-3.70577, 
+		default_locations.add(LandmarkFactory.getLandmark("Japan, Tokyo", "", new QualifiedCoordinates(35.689488, 139.691706, 0f, Float.NaN, Float.NaN), Commons.LOCAL_LAYER, installed)); //Japan, Tokyo, 35.689488,139.691706 
+		default_locations.add(LandmarkFactory.getLandmark("United Kingdom, London", "", new QualifiedCoordinates(51.506321, -0.12714, 0f, Float.NaN, Float.NaN), Commons.LOCAL_LAYER, installed)); //United Kingdom, London, 51.506321,-0.12714  
+    	default_locations.add(LandmarkFactory.getLandmark("India, Mumbai", "", new QualifiedCoordinates(19.076191, 72.875877, 0f, Float.NaN, Float.NaN), Commons.LOCAL_LAYER, installed)); //India, Mumbai, 19.076191,72.875877 
+    	default_locations.add(LandmarkFactory.getLandmark("China, Beijing", "", new QualifiedCoordinates(39.90403, 116.407526, 0f, Float.NaN, Float.NaN), Commons.LOCAL_LAYER, installed)); //China, Beijing 39.90403, 116.407526
+    	default_locations.add(LandmarkFactory.getLandmark("Poland, Warsaw", "", new QualifiedCoordinates(52.235352, 21.00939, 0f, Float.NaN, Float.NaN), Commons.LOCAL_LAYER, installed)); //Poland, Warsaw, 52.235352,21.00939
+    	default_locations.add(LandmarkFactory.getLandmark("Canada, Toronto", "", new QualifiedCoordinates(43.64856, -79.38533, 0f, Float.NaN, Float.NaN), Commons.LOCAL_LAYER, installed)); //Canada, Toronto, 43.64856,-79.38533
+    	default_locations.add(LandmarkFactory.getLandmark("Brazil, Sao Paolo", "", new QualifiedCoordinates(-23.548943, -46.638818, 0f, Float.NaN, Float.NaN), Commons.LOCAL_LAYER, installed)); //Brazil, Sao Paolo -23.548943,-46.638818,     
+    	default_locations.add(LandmarkFactory.getLandmark("Indonesia, Jakarta", "", new QualifiedCoordinates(-6.17144, 106.82782, 0f, Float.NaN, Float.NaN), Commons.LOCAL_LAYER, installed)); //IDN Indonesia, Jakarta -6.17144, 106.82782
+    	default_locations.add(LandmarkFactory.getLandmark("Thailand, Bangkok", "", new QualifiedCoordinates(13.75333, 100.504822, 0f, Float.NaN, Float.NaN), Commons.LOCAL_LAYER, installed)); //THA Thailand, Bangkok 13.75333, 100.504822
+    	default_locations.add(LandmarkFactory.getLandmark("Russia, Moscow", "", new QualifiedCoordinates(55.755786, 37.617633, 0f, Float.NaN, Float.NaN), Commons.LOCAL_LAYER, installed)); //RUS Russia, Moscow 55.755786, 37.617633
+    	default_locations.add(LandmarkFactory.getLandmark("Mexico, Mexico City", "", new QualifiedCoordinates(19.432608, -99.133208, 0f, Float.NaN, Float.NaN), Commons.LOCAL_LAYER, installed)); //MEX Mexico, Mexico City 19.432608, -99.133208
+    	default_locations.add(LandmarkFactory.getLandmark("Malaysia, Kuala Lumpur", "", new QualifiedCoordinates(3.15248, 101.71727, 0f, Float.NaN, Float.NaN), Commons.LOCAL_LAYER, installed)); //MYS Malaysia, Kuala Lumpur 3.15248, 101.71727
+    	default_locations.add(LandmarkFactory.getLandmark("Turkey, Istanbul", "", new QualifiedCoordinates(41.00527, 28.97696, 0f, Float.NaN, Float.NaN), Commons.LOCAL_LAYER, installed)); //TUR Turkey, Istanbul 41.00527, 28.97696
+    	default_locations.add(LandmarkFactory.getLandmark("Philippines, Manilia", "", new QualifiedCoordinates(14.5995124, 120.9842195, 0f, Float.NaN, Float.NaN), Commons.LOCAL_LAYER, installed)); //PHL Philippines, Manilia 14.5995124, 120.9842195
+    	default_locations.add(LandmarkFactory.getLandmark("Netherlands, Amsterdam", "", new QualifiedCoordinates(52.373119, 4.89319, 0f, Float.NaN, Float.NaN), Commons.LOCAL_LAYER, installed)); //NLD Netherlands, Amsterdam 52.373119, 4.89319
+    	default_locations.add(LandmarkFactory.getLandmark("Saudi Arabia, Riyadh", "", new QualifiedCoordinates(24.64732, 46.714581, 0f, Float.NaN, Float.NaN), Commons.LOCAL_LAYER, installed)); //SAU Saudi Arabia, Riyadh 24.64732, 46.714581
+    	default_locations.add(LandmarkFactory.getLandmark("Portugal, Lisbon", "", new QualifiedCoordinates(38.7252993, -9.1500364, 0f, Float.NaN, Float.NaN), Commons.LOCAL_LAYER, installed)); //PRT Portugal, Lisbon 38.7252993, 9.1500364
+    	default_locations.add(LandmarkFactory.getLandmark("Pakistan, Islamabad", "", new QualifiedCoordinates(33.718151, 73.060547, 0f, Float.NaN, Float.NaN), Commons.LOCAL_LAYER, installed)); //PAK Pakistan, Islamabad 33.718151, 73.060547
+    	default_locations.add(LandmarkFactory.getLandmark("Sweden, Stockholm", "", new QualifiedCoordinates(59.32893, 18.06491, 0f, Float.NaN, Float.NaN), Commons.LOCAL_LAYER, installed)); //SWE Sweden, Stockholm 59.32893, 18.06491  	       	
+  	
+    	markerCluster.addMarkers(default_locations);
+	}
+	
+	private static class LoadingHandler extends Handler {
+		
+		private WeakReference<GMSClient3MainActivity> parentActivity;
+    	
+    	public LoadingHandler(GMSClient3MainActivity parentActivity) {
+    		this.parentActivity = new WeakReference<GMSClient3MainActivity>(parentActivity);
+    	}
+		
+		@Override
+        public void handleMessage(Message msg) {
+			GMSClient3MainActivity activity = parentActivity.get();
+        	if (activity != null && !activity.isFinishing()) {
+        		if (msg.what == SHOW_MAP_VIEW) {
+                	activity.findViewById(R.id.mapCanvasWidgetL).setVisibility(View.GONE);
+                	activity.findViewById(R.id.mapContainer).setVisibility(View.VISIBLE);
+                	//TODO uncomment
+                	//if (activity.lvView == null || !activity.lvView.isShown()) {
+                	if (activity.getSupportActionBar() != null) {
+                		activity.getSupportActionBar().show();
+                	}	
+                	//}
+            	}
+        	}
+			
+		}
+	}
+
+}
---
+++ GMSClientNew/src/com/jstakun/gms/android/ui/NavigationDrawerExpandableListAdapter.java
@@ -0,0 +1,189 @@
+package com.jstakun.gms.android.ui;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//import com.jstakun.gms.android.config.Commons;
+//import com.jstakun.gms.android.config.ConfigurationManager;
+//import com.jstakun.gms.android.data.FileManager;
+//import com.jstakun.gms.android.data.FilenameFilterFactory;
+//import com.jstakun.gms.android.data.PersistenceManagerFactory;
+//import com.jstakun.gms.android.landmarks.LandmarkManager;
+//import com.jstakun.gms.android.utils.ProjectionInterface;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.TextView;
+
+public class NavigationDrawerExpandableListAdapter extends BaseExpandableListAdapter {
+
+	private Activity parentActivity;
+	private List<NavigationDrawerListItem> parents = new ArrayList<NavigationDrawerListItem>();
+	private List<NavigationDrawerListItem> landmarks = new ArrayList<NavigationDrawerListItem>();
+	private List<NavigationDrawerListItem> checkins = new ArrayList<NavigationDrawerListItem>(); 
+	private static final int CHECKIN = -1;
+	private static final int LANDMARK = -2;
+	//private IntentsHelper intents;
+	
+	public NavigationDrawerExpandableListAdapter(Activity parent) {
+		this.parentActivity = parent;
+		//intents = new IntentsHelper(parent, null, null);
+		rebuild();
+	}
+	
+	public void rebuild() {
+		String[] parentGroup = parentActivity.getResources().getStringArray(R.array.navigation);
+		String[] landmark = parentActivity.getResources().getStringArray(R.array.landmarks_submenu);
+		String[] checkin = parentActivity.getResources().getStringArray(R.array.checkin_submenu);
+	
+		//0 <item>Layers</item>
+        //1 <item>Landmarks</item> <!-- submenu -->
+        //2 <item>Check-in</item> <!-- submenu -->
+        //3 <item>Friends activities</item>
+		//4 <item>Deals</item>
+        //5 <item>Social Networks</item>
+		
+		parents.clear();	    
+	    parents.add(new NavigationDrawerListItem(parentGroup[0], R.id.showLayers));
+	    parents.add(new NavigationDrawerListItem(parentGroup[1], LANDMARK));
+	    parents.add(new NavigationDrawerListItem(parentGroup[2], CHECKIN));
+	    //if (landmarkManager != null && landmarkManager.hasFriendsCheckinLandmarks()) {
+	    	parents.add(new NavigationDrawerListItem(parentGroup[3], R.id.friendsCheckins));
+	    //}
+        //if (landmarkManager != null && landmarkManager.hasDeals()) {
+        	parents.add(new NavigationDrawerListItem(parentGroup[4], R.id.deals));
+        //}
+        parents.add(new NavigationDrawerListItem(parentGroup[5], R.id.socialNetworks));
+	
+	    //<item>Nearby landmarks</item>
+    	//<item>Create new landmark</item>
+    	//<item>Recently opened</item>
+    	//<item>Newest landmarks</item>
+    	//<item>Saved landmarks</item>
+    	//<item>Calendar</item>
+    	//<item>Poi files</item>
+    	
+	    landmarks.clear();
+	    
+	    //if (landmarkManager != null && projection != null && landmarkManager.hasVisibleLandmarks(projection, true)) {
+	    	landmarks.add(new NavigationDrawerListItem(landmark[0], R.id.listLandmarks));
+	    //}
+	    landmarks.add(new NavigationDrawerListItem(landmark[1], R.id.addLandmark));
+	    
+	    //if (landmarkManager != null && landmarkManager.hasRecentlyOpenedLandmarks()) {
+	    	landmarks.add(new NavigationDrawerListItem(landmark[2], R.id.recentLandmarks));
+	    //}
+	    
+	    //final String[] excluded = new String[]{Commons.MY_POSITION_LAYER, Commons.ROUTES_LAYER};
+	    //if (landmarkManager != null && landmarkManager.hasNewLandmarks(2, excluded)) {
+	    	landmarks.add(new NavigationDrawerListItem(landmark[3], R.id.newestLandmarks));
+	    //}
+	    
+	    landmarks.add(new NavigationDrawerListItem(landmark[4], R.id.showMyLandmarks));
+	    
+	    landmarks.add(new NavigationDrawerListItem(landmark[5], R.id.events));
+	    //if (!PersistenceManagerFactory.getFileManager().isFolderEmpty(FileManager.getFileFolderPath(), FilenameFilterFactory.getFilenameFilter("kml"))) {
+	    	landmarks.add(new NavigationDrawerListItem(landmark[6], R.id.loadPoiFile)); 
+	    //}
+	    
+	    //<item>Auto Check-In</item>
+        //<item>Check-in at location</item>
+        //<item>Check-in with QR code</item>
+    	
+	    checkins.clear();
+	    checkins.add(new NavigationDrawerListItem(checkin[0], R.id.autocheckin));
+	    
+	    //if (landmarkManager != null && landmarkManager.hasCheckinableLandmarks()) {
+	    	checkins.add(new NavigationDrawerListItem(checkin[1], R.id.searchcheckin));
+	    //}
+	    
+	    //if (intents.isIntentAvailable(IntentsHelper.SCAN_INTENT)) {
+	    	checkins.add(new NavigationDrawerListItem(checkin[2], R.id.qrcheckin));
+	    //}
+	    
+	    notifyDataSetChanged();
+	}
+	
+	@Override
+	public Object getChild(int groupPosition, int childPosition) {
+		return null;
+	}
+
+	@Override
+	public long getChildId(int groupPosition, int childPosition) {
+		if (parents.get(groupPosition).getResource() == LANDMARK) {
+			return landmarks.get(childPosition).getResource();
+		} else if (parents.get(groupPosition).getResource() == CHECKIN) {
+			return checkins.get(childPosition).getResource();
+		} else {
+			return 0;
+		}
+	}
+
+	@Override
+	public View getChildView(final int groupPosition, final int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+		View rowView = convertView;
+        if (rowView == null) {
+            LayoutInflater inflater = parentActivity.getLayoutInflater();
+            rowView = inflater.inflate(R.layout.drawerrow_child, null, true);
+        }    
+        if (parents.get(groupPosition).getResource() == LANDMARK) {
+        	((TextView)rowView).setText(landmarks.get(childPosition).getName());
+        } else if (parents.get(groupPosition).getResource() == CHECKIN) {
+        	((TextView)rowView).setText(checkins.get(childPosition).getName());
+        } 
+      	return rowView;
+	}
+
+	@Override
+	public int getChildrenCount(int position) {
+		if (parents.get(position).getResource() == LANDMARK) {
+			return landmarks.size();
+		} else if (parents.get(position).getResource() == CHECKIN) {
+			return checkins.size();
+		} else {
+			return 0;
+		}
+	}
+
+	@Override
+	public Object getGroup(int arg0) {
+		return null;
+	}
+
+	@Override
+	public int getGroupCount() {
+		return parents.size();
+	}
+
+	@Override
+	public long getGroupId(int position) {
+		return parents.get(position).getResource();
+	}
+
+	@Override
+	public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+		View rowView = convertView;
+        if (rowView == null) {
+            LayoutInflater inflater = parentActivity.getLayoutInflater();
+            rowView = inflater.inflate(R.layout.drawerrow_parent, null, true);
+        }    
+        TextView textView = (TextView)rowView;
+        textView.setText(parents.get(groupPosition).getName());
+        return rowView;
+	}
+
+	@Override
+	public boolean hasStableIds() {
+		return true;
+	}
+
+	@Override
+	public boolean isChildSelectable(int arg0, int arg1) {
+		return true;
+	}
+
+}
