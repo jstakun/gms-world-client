6763df997db2fdcababbb60abddcb09f7c6036f3
jstakun <jstakun@gms-world.net>
2016-08-04 11:24:15 +0200

    minor ui modification

--- GMSClient3/src/com/jstakun/gms/android/ui/GMSClient2MainActivity.java
+++ GMSClient3/src/com/jstakun/gms/android/ui/GMSClient2MainActivity.java
@@ -1171,6 +1171,7 @@ public class GMSClient2MainActivity extends MapActivity implements OnClickListen
             addOverlay(landmarkOverlay);*/
         	//TODO testing
         	markerCluster = new OsmMarkerClusterOverlay(this, landmarkManager, loadingHandler);
+        	markerCluster.clearMarkers();
         	for (String layer : landmarkManager.getLayerManager().getLayers()) {
         		if (landmarkManager.getLayerManager().getLayer(layer).getType() != LayerManager.LAYER_DYNAMIC && landmarkManager.getLayerManager().getLayer(layer).isEnabled() && landmarkManager.getLayerSize(layer) > 0) {
         			markerCluster.addMarkers(layer, (org.osmdroid.views.MapView)mapView);
