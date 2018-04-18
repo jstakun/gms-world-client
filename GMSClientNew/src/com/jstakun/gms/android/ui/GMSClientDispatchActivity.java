05e3ce77a7912d7b044f726b388ae31cd2788e63
gmsclient <gmsclient@mobile>
2018-04-18 09:13:47 +0200

    testing

--- GMSClientNew/src/com/jstakun/gms/android/ui/GMSClientDispatchActivity.java
+++ GMSClientNew/src/com/jstakun/gms/android/ui/GMSClientDispatchActivity.java
@@ -66,7 +66,7 @@ public class GMSClientDispatchActivity extends Activity {
                 			lat = StringUtil.decode(latSegment);
                 			String lngSegment = StringUtils.split(data.getPathSegments().get(length-1), ";jsessionid=")[0]; 
                 			lng = StringUtil.decode(lngSegment);
-                			LoggerUtils.debug("Decoded params " + lat + "," + lng);
+                			LoggerUtils.debug("Decoded params " + latSegment + "," + lngSegment + " to " + lat + "," + lng);
                 		} catch (Exception e) {
                 			LoggerUtils.debug("Unable to decode " + data.getPathSegments().get(length-2) + "," + data.getPathSegments().get(length-1));
                 		}
